a4b92b6fbc0b2d289c8b23318bd16c1511adf039
Eduardo Pérez <eduardojoseperezf@gmail.com>
2019-03-24 12:42:22 -0300

    Vista Paso 2

--- web/resources/views/pasantia/paso2.blade.php
+++ web/resources/views/pasantia/paso2.blade.php
@@ -20,7 +20,44 @@
 	<div class="row justify-content-md-center mb-5">
 		<form method="post" action="{{ route('inscripcion.2.post') }}">
 			@csrf
-			<button type="submit" class="btn btn-primary">Continuar</button>
+			<div class="form-group">
+		    <label for="empresa">Empresa en la que trabajarás</label>
+		    <select class="form-control" id="empresa" name="empresa">
+		      <option>Empresa A</option>
+		      <option>Empresa B</option>
+		      <option>Empresa C</option>
+		      <option>Empresa D</option>
+		      <option>Empresa E</option>
+		    </select>
+		  </div>
+			<div class="form-group">
+		    <label for="ciudad">Ciudad en la que harás la pasantía</label>
+		    <input class="form-control" id="ciudad" name="ciudad" placeholder="Ciudad">
+		  </div>
+			<div class="form-group">
+		    <label for="pais">País en la que harás la pasantía</label>
+		    <input class="form-control" id="pais" name="pais" placeholder="País" required>
+		  </div>
+			<div class="form-group">
+				<label for="fecha">Fecha en la que iniciarás tu pasantía</label>
+				<input class="form-control" type="date" name="fecha" id="fecha" required>
+			</div>
+			<div class="form-group">
+		    <label for="horas">Horas semanales de trabajo</label>
+		    <input type="number" class="form-control" id="horas" name="horas" required>
+		  </div>
+			<p>Tengo un pariente que trabaja en la empresa</p>
+			<div class="form-check form-check-inline">
+			  <input class="form-check-input" type="radio" name="pariente" id="parienteno" value="0" checked>
+			  <label class="form-check-label" for="parienteno">No</label>
+			</div>
+			<div class="form-check form-check-inline">
+
+			  <input class="form-check-input" type="radio" name="pariente" id="parientesi" value="1">
+			  <label class="form-check-label" for="parientesi">Sí</label>
+			</div>
+			</br>
+			<button type="submit" class="btn btn-primary mt-3">Continuar</button>
 		</form>
 	</div>
 </div>
