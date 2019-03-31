553a052db9168495ba3b678eef81e15762a3dfd9
niniDev <31903811+niniDev@users.noreply.github.com>
2019-03-30 22:16:05 -0300

    Update Graficos
    Eliminados graficos Pie y Lineal
    Eliminado el texto Highcharts abajo a la derecha del grafico

--- web/resources/views/admin/index.blade.php
+++ web/resources/views/admin/index.blade.php
@@ -15,9 +15,8 @@
 
 		<div class="row">
 			<div class="col-xs-12">
-				<button type="button" id="barBtn" class="btn btn-primary">Barras</button>
-				<button type="button" id="pieBtn" class="btn btn-primary">Pie</button>
-				<button type="button" id="linBtn" class="btn btn-primary">Lineal</button>
+				<button type="button" id="barBtn" class="btn btn-primary">Graficar</button>
+
 			</div>
 		</div>
 		<div class="row">
@@ -32,58 +31,14 @@
 
 
 <script>
-	document.getElementById("pieBtn").addEventListener("click", function() {
-		var pieChart = Highcharts.chart('grafico', {
-			chart: {
-				plotBackgroundColor: null,
-				plotBorderWidth: null,
-				plotShadow: false,
-				type: 'pie'
-			},
-			title: {
-				text: 'Cantidad de alumnos en cada paso en el proceso de inscripcion de pasantia en el mes X'
-			},
-
-			plotOptions: {
-				pie: {
-					allowPointSelect: true,
-					cursor: 'pointer',
-					dataLabels: {
-						enabled: true,
-						format: '<b>{point.name}</b>: {point.percentage:.2f} %',
-						style: {
-							color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-						}
-					}
-				}
-			},
-			series: [{
-				name: 'Proceso de pasantia',
-				colorByPoint: true,
-				data: [{
-					name: 'Paso 1',
-					y: 43.15,
-					sliced: true,
-					selected: true
-				}, {
-					name: 'Paso 2',
-					y: 22.31
-				}, {
-					name: 'Paso 3',
-					y: 18.76
-				}, {
-					name: 'Paso 4',
-					y: 15.78
-				}]
-			}]
-		});
-	});
-
 	document.getElementById("barBtn").addEventListener("click", function() {
 		var barChart = Highcharts.chart('grafico', {
 			chart: {
 				type: 'bar'
 			},
+			credits: {
+      enabled: false
+  		},
 			title: {
 				text: 'Cantidad de alumnos en cada paso en el proceso de inscripcion de pasantia'
 			},
@@ -99,42 +54,6 @@
 				}
 			},
 
-			series: [{
-					name: 'Paso 1',
-					data: [70, 40, 30, 20, 2]
-				}, {
-					name: 'Paso 2',
-					data: [15, 30, 25, 20, 3]
-				},
-				{
-					name: 'Paso 3',
-					data: [10, 15, 20, 20, 5]
-				},
-				{
-					name: 'Paso 4',
-					data: [5, 15, 25, 40, 90]
-				}
-			]
-		});
-	});
-
-	document.getElementById("linBtn").addEventListener("click", function() {
-		var linChart = Highcharts.chart('grafico', {
-
-			title: {
-				text: 'Cantidad de alumnos en cada paso en el proceso de inscripcion de pasantia'
-			},
-			xAxis: {
-				title: {
-					text: 'Mes'
-				},
-				categories: ['Mar', 'Abr', 'May', 'Jun', 'Jul']
-			},
-			yAxis: {
-				title: {
-					text: 'Cantidad de postulantes'
-				}
-			},
 			series: [{
 					name: 'Paso 1',
 					data: [70, 40, 30, 20, 2]
