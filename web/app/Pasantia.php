e83d780d36e90ea0b5945fd24c6142faab018a4b
Eduardo Pérez <eduardojoseperezf@gmail.com>
2019-04-08 11:01:30 -0400

    Modelo Pasantías

--- web/app/Pasantia.php
+++ web/app/Pasantia.php
@@ -7,5 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class Pasantia extends Model{
 	protected $table = 'pasantia';
 	protected $primaryKey = 'idPasantia';
-    //
+
+	protected $fillable = [
+		'idAlumno',
+		'idProfesor',
+		'fechaInicio',
+		'idEmpresa',
+		'nombreJefe',
+		'correoJefe',
+		'lecReglamento',
+		'practicaOp',
+		'ciudad',
+		'pais',
+		'horasSemanales',
+		'parienteEmpresa',
+	]
 }
