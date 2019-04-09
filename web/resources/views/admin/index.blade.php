be656013dd601e4193014a8a5a67ae12bd7013ad
niniDev <31903811+niniDev@users.noreply.github.com>
2019-04-09 00:08:24 -0400

    Update index.blade.php
    Grafico Barras
    Fecha actual en subtitulo
    Nombres actualizados

--- web/resources/views/admin/index.blade.php
+++ web/resources/views/admin/index.blade.php
@@ -10,7 +10,7 @@
 		</div><br />
 		@endif
 		<div class="row">
-			<h1>Gráfico de número de estudiantes en cada paso</h1>
+			<h1>Estadisticas Administrativas</h1>
 		</div>
 
 		<div class="row">
@@ -23,117 +23,97 @@
 
 
 <script>
-//SCRIPT CREA GRAFICO Y DATA RANDOM, FECHAS INCAMBIABLES
-// CREA GRAFICO
-$(function () {
-	//FECHA RANDOM
-	function randomDate(start, end) {
-		//FECHA INCAMBIABLE
-		return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()))
-	}
-	//DATA RANDOM
-	function randomData(){
-		var arr = [];
-		//DATOS DESDE FECHA INICIAL A FECHA FINAL
-		for (var i = 0; i < 100; i++) {
-			var date=randomDate(new Date(2004, 0, 9), new Date());
-			console.log(date);
-			var randNum=Math.round(Math.random() * 100);
 
-			arr.push([date.getTime(), randNum]);
+//FECHA ACTUAL
+function fecha(){
+	const hoy = new Date();
+	let date = hoy.getFullYear()+'-'+(hoy.getMonth()+1)+'-'+hoy.getDate();
+	return date
+}
+
+window.chart = new Highcharts.chart({
+	//EN DONDE UBICARLO
+	chart: {
+		type: 'bar',
+		renderTo: 'grafico',
+		height: (9 / 16 * 100) + '%'
+	},
+
+	//TITULO
+	title: {
+		text: 'Porcentaje de postulantes en cada paso '
+	},
+	//SUBTITULO, FECHA ACTUAl
+	subtitle: {
+		text: 'Actualizado a la fecha: ' + fecha()
+	},
+
+	//BOTONES DE DESCARGA
+	exporting: {
+		enabled: true,
+		csv: {
+			dateFormat:'%A, %b %e, %Y'
 		}
-		arr.sort(function (a,b) {
-			if (a[0] < b[0]) return -1;
-			if (a[0]> b[0]) return 1;
-			return 0;
-		})
+	},
 
-		return arr;
-	}
+	//SACAR CREDITOS
+	credits: {
+		enabled: false
+	},
 
-	window.chart = new Highcharts.StockChart({
-		//EN DONDE UBICARLO
-		chart: {
-			renderTo: 'grafico',
-			height: (9 / 16 * 100) + '%'
-		},
+	//LABEL EJE X
+	xAxis: {
+		type: 'category'
+	},
+
+	//LABEL EJE Y
+	yAxis: {
+		min: 0,
+		max: 100,
 		title: {
-			text: 'Fecha y Número de estudiantes en cada paso'
+			text: 'Porcentaje de postulantes',
+			align: 'high'
 		},
-		exporting: {
-			enabled: true,
-			csv: {
-				dateFormat:'%A, %b %e, %Y'
+		labels: {
+			overflow: 'justify'
+		}
+	},
+
+	//COLORES Y LABEL DE CADA COLUMNA
+	plotOptions: {
+		series: {
+			colorByPoint: true,
+			dataLabels: {
+				enabled: true,
+				inside: true
 			}
-		},
-		//SACAR CREDITOS
-		credits: {
-			enabled: false
-		},
-		tooltip:{
-			shared: true,
-			xDateFormat: '%A, %b %e, %Y'
-		},
-		//LABEL EJE X
-		xAxis: {
-			title: {
-				text: 'Fecha'
-			}
-		},
-		//LABEL EJE Y
-		yAxis: {
-			title: {
-				text: 'Cantidad de postulantes'
-			}
-		},
-		//LINEAS Y DATOS RESPECTIVOS
-		series: [{
-			name: 'Postulantes Paso 1',
-			data: randomData()
-		},
-		{
-			name: 'Postulantes Paso 2',
-			data: randomData()
-		},
-		{
-			name: 'Postulantes Paso 3',
-			data: randomData()
-		},
-		{
-			name: 'Postulantes Paso 4',
-			data: randomData()
+		}
+	},
+
+	//DATA
+	series: [{
+		name: 'Postulantes',
+		dataLabels: [{
+			align: 'right',
+			format: '{y} %'
 		}],
-
-		//BOTONES
-		rangeSelector: {
-			buttons: [{
-				type: 'month',
-				count: 1,
-				text: '1M',
-			}, {
-				type: 'month',
-				count: 3,
-				text: '3M'
-			}, {
-				type: 'month',
-				count: 6,
-				text: '6M'
-			},  {
-				type: 'year',
-				count: 1,
-				text: '1A'
-			},
-			{
-				type: 'ytd',
-				text: 'YTD'
-			}, {
-				type: 'all',
-				text: 'Todo'
-			}]
-		},
-
-	});
+		data: [{
+			y: 30,
+			name: 'Postulantes Paso 1'
+		}, {
+			y: 20,
+			name: 'Postulantes Paso 2'
+		}, {
+			y: 40,
+			name: 'Postulantes Paso 3',
+		}, {
+			y: 10,
+			name: 'Postulantes Paso 4',
+		}],
+		showInLegend: false
+	}]
 });
+
 </script>
 
 
