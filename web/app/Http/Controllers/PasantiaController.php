198205ddee28fa76322cdbc91039e277624e1560
Eduardo Pérez <eduardojoseperezf@gmail.com>
2019-04-20 19:25:00 -0400

    Logica Resumen
    TODO: Inscripción de proyecto

--- web/app/Http/Controllers/PasantiaController.php
+++ web/app/Http/Controllers/PasantiaController.php
@@ -229,12 +229,16 @@ class PasantiaController extends Controller{
 	public function resumenView(){
 		$userId = Auth::id();
 		$pasantia = Pasantia::where('idAlumno', $userId)->first();
+		$empresa = Empresa::where('idEmpresa', $pasantia->idEmpresa)->first();
+
 		return view('pasantia.resumen', [
 			'statusPaso0'=>$pasantia->statusPaso0,
 			'statusPaso1'=>$pasantia->statusPaso1,
 			'statusPaso2'=>$pasantia->statusPaso2,
 			'statusPaso3'=>$pasantia->statusPaso3,
-			'statusPaso4'=>$pasantia->statusPaso4]);
+			'statusPaso4'=>$pasantia->statusPaso4,
+			'pasantia'=>$pasantia,
+			'empresa'=>$empresa]);
 	}
 
 }
