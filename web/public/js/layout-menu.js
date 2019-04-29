f83ebaf58a64bb972c9bb37e83f785e501724016
niniDev <31903811+niniDev@users.noreply.github.com>
2019-04-29 13:29:51 -0400

    Update layout-menu.js

--- web/public/js/layout-menu.js
+++ web/public/js/layout-menu.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6 */
+
 // JQUERY Colapsar menu al clikear boton sidebarCollapse
 $(document).ready(function () {
   $('#sidebarCollapse').on('click', function () {
@@ -13,5 +15,56 @@ function fecha(){
   return fecha;
 }
 function appendFecha(elementID){
-	document.getElementById(elementID).innerHTML += fecha();
+  document.getElementById(elementID).innerHTML += fecha();
 }
+
+/*
+function grafico(tipo, objetivo, titulo, nombreEjeY, ){
+   window.chart = new Highcharts.chart({
+     chart: {
+       type: tipo,
+       renderTo: objetivo,
+       height: (9/16 * 75) + '%'
+     },
+
+     title: {
+       text: titulo,
+       style: {
+         fontSize: '2em'
+       }
+     },
+
+     exporting: {
+       enabled: true,
+       csv: {
+         dateFormat: '%A, %b %e, %Y'
+       }
+     },
+
+     credits: {
+       enabled: false
+     },
+
+     xAxis: {
+       type: 'category',
+       labels: {
+         style:{
+           fontSize: '1.25em',
+           fontWeight: 'bold'
+         }
+       }
+     },
+
+     yAxis: {
+       min: 0,
+       max: 100,
+       title: {
+        text: nombreEjeY,
+        style: {
+
+        }
+       }
+     }
+   });
+ }
+*/
