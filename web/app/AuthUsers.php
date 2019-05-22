2cef15370c74723843c8b9280bcdafd336a9d18a
Eduardo Pérez <eduardojoseperezf@gmail.com>
2019-05-21 21:44:10 -0400

    Base comprobación de alumnos autorizados

---
+++ web/app/AuthUsers.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AuthUsers extends Model
+{
+  protected $table = 'authUsers';
+	protected $primaryKey = 'idAuthUsers';
+	public $timestamps = false;
+	protected $fillable = [
+		'email'
+	];
+}
