a22f3691288f69b6173cef62a1f0df9c37c0fc36
nicolasarciat <nicolasagarciat@mail.com>
2019-06-10 13:42:18 -0400

    rutas, controller y vista creadas para el formulario

---
+++ web/resources/views/evalTutor/formulario.blade.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Evaluacion de tutor</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+            <div class="content">
+                <div class="title m-b-md">
+                    Evaluacion: <?php echo $id; ?>
+                </div>
+                <form action="/evaluacion/<?php echo $id; ?>" method="post" id="search">
+                    @csrf
+                    <input type="text" name="compromiso">
+                    <input type="text" name="adaptabilidad">
+                    <input type="text" name="comunicacion">
+                    <input type="text" name="equipo">
+                    <input type="text" name="liderazgo">
+                    <input type="text" name="sobreponerse">
+                    <input type="text" name="habilidades">
+                    <input type="text" name="proactividad">
+                    <input type="text" name="innovacion">
+                    <input type="text" name="etica">
+                    <input type="textarea" name="comentario">
+                    <input type="submit" name="enviar">
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
--- web/routes/web.php
+++ web/routes/web.php
@@ -40,3 +40,6 @@ Route::post('/inscripcion/4/post','PasantiaController@paso4Control')->name('insc
 Route::get('/inscripcion/resumen', 'PasantiaController@resumenView')->name('inscripcion.resumen')->middleware('auth');
 
 Route::get('/inscripcion/resumen','PasantiaController@resumenView')->name('inscripcion.resumen');
+
+Route::post('evaluacion/{id}','EvalTutorController@save')->name('evalTutor.save');
+Route::get('evaluacion/{id}', 'EvalTutorController@show')->name('evalTutor.show');
