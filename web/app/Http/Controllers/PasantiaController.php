8c58ef27a78edba7da00526af56a1fc2f8ae4580
Eduardo Pérez <eduardojoseperezf@gmail.com>
2019-06-11 03:40:48 -0400

    Hotfix validación 2

--- web/app/Http/Controllers/PasantiaController.php
+++ web/app/Http/Controllers/PasantiaController.php
@@ -182,7 +182,7 @@ class PasantiaController extends Controller{
 			'horas' => 'integer|between:25,45|nullable',
 			'pariente' => 'boolean|nullable',
 			'otraEmpresa' => 'boolean|nullable',
-			'rolPariente' => 'required_if:pariente,1|alpha'
+			'rolPariente' => 'required_if:pariente,1'
 		]);
 		$userId = Auth::id();
 		$pasantia = Pasantia::where('idAlumno', $userId)->first();
