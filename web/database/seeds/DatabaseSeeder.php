30273b7aea367a8ce769820e615398f3c3d13a1f
Eduardo Pérez <eduardojoseperezf@gmail.com>
2019-07-04 21:24:25 -0400

    Seed empresas y usuarios iniciales

--- web/database/seeds/DatabaseSeeder.php
+++ web/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(EmpresaTableSeeder::class);
+        $this->call([
+					EmpresaTableSeeder::class,
+					AuthUsersTableSeeder::class
+				]);
     }
 }
