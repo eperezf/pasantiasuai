9aaf22a27a0ba733ff521bb9277e5328e68b7f54
IgnacioZentenoSmith <31903811+IgnacioZentenoSmith@users.noreply.github.com>
2019-08-12 09:14:38 -0400

    Revert "#91 Ahora no tiene límite de caracteres"
    This reverts commit 5cdd8e00a30201ed21ba906660b3ecd4791d5f38.

--- web/database/migrations/2019_03_14_011547_create_empresa_table.php
+++ web/database/migrations/2019_03_14_011547_create_empresa_table.php
@@ -8,10 +8,10 @@ class CreateEmpresaTable extends Migration{
 	public function up(){
     Schema::create('empresa', function (Blueprint $table) {
       $table->increments('idEmpresa');
-      $table->string('nombre', 150);
-      $table->string('rubro', 150);
-      $table->string('urlWeb', 150);
-      $table->string('correoContacto', 150);
+      $table->string('nombre', 200);
+      $table->string('rubro', 250);
+      $table->string('urlWeb', 200);
+      $table->string('correoContacto', 200);
 			$table->integer('status');
 			$table->timestamps();
 
