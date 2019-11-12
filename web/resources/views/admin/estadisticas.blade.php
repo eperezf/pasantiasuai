d64e9a99ec4b7bb31f6040f4fc94d2eba6f18f2f
Ignacio Zenteno Smith <31903811+IgnacioZentenoSmith@users.noreply.github.com>
2019-11-12 19:01:06 -0300

    Refactorizacion grafico de torta
    Modificado a un constructor generico
    Ahora los graficos se generan a partir del unico constructor con los datos requeridos

--- web/resources/views/admin/estadisticas.blade.php
+++ web/resources/views/admin/estadisticas.blade.php
@@ -22,10 +22,18 @@
 		</div>
 		<div class="row">
 			<div class="col-md-6">
-				<div id="defensas"></div>
+				<div id="proyectosValidados"></div>
 			</div>
 			<div class="col-md-6">
-				<div id="empresas"></div>
+				<div id="empresasEnConvenio"></div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-6">
+				<div id="proyectosValidados"></div>
+			</div>
+			<div class="col-md-6">
+				<div id="empresasEnConvenio"></div>
 			</div>
 		</div>
 
@@ -184,7 +192,7 @@ window.chart = new Highcharts.chart({
 			format: '{y} '
 		},{
 			align: 'center',
-			format: '{point.porcentajePostulantes} %'
+			format: '{point.porcentajePostulantes:.2f} %'
 		}],
 		//Datos
 		data: [{
@@ -212,243 +220,129 @@ window.chart = new Highcharts.chart({
 	}]
 });
 
-//Grafico de estado de pasantisa y defensas de los alumnos
-window.chart = new Highcharts.chart({
-	//EN DONDE UBICARLO
-	chart: {
-		type: 'pie',
-		renderTo: 'defensas',
-		height: (9 / 16 * 100) + '%',
-		plotBackgroundColor: null,
-		plotBorderWidth: null,
-		plotShadow: false
-	},
-	//TITULO
-	title: {
-		text: 'Estado de pasantías y defensas ',
-		style: {
-			fontSize: '22px'
-		}
-	},
-	//TOOLTIP
-	tooltip: {
-		pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-	},
-	//BOTONES DE DESCARGA
-	exporting: {
-		enabled: true,
-		csv: {
-			dateFormat:'%A, %b %e, %Y'
-		}
-	},
-	//SACAR CREDITOS
-	credits: {
-		enabled: false
-	},
-	//COLORES Y LABEL DE CADA PARTE
-  plotOptions: {
-    pie: {
-      allowPointSelect: true,
-      dataLabels: {
-        enabled: true,
-        format: '<b>{point.name}</b>: {point.cantidadPasantias}',
-        style: {
-          color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black',
-					fontSize: '1em'
-        }
-      }
-    },
-		series: {
-      cursor: 'pointer',
-			point: {
-				events: {
-					click: function (e) {
-						hs.htmlExpand(null, {
-							pageOrigin: {
-								x: e.pageX || e.clientX,
-								y: e.pageY || e.clientY
-							},
-							headingText: this.series.data[this.x].name,
-							maincontentText:
-							// TABLA FIJA -- DUMMY
-							'<table class="table table-striped">' +
-							'<thead>' +
-							'<tr>' +
-							'<th scope="col">#</th>' +
-							'<th scope="col">Nombre</th>' +
-							'<th scope="col">Apellido</th>' +
-							'<th scope="col">Carrera</th>' +
-							'</tr>' +
-							'</thead>' +
-							'<tbody>' +
-							'<tr>' +
-							'<th scope="row">1</th>' +
-							'<td>Jaime</td>' +
-							'<td>Maxwell</td>' +
-							'<td>Derecho</td>' +
-							'</tr>' +
-							'<tr>' +
-							'<th scope="row">2</th>' +
-							'<td>Juana</td>' +
-							'<td>Thomson</td>' +
-							'<td>Ingeniería Comercial</td>' +
-							'</tr>' +
-							'<tr>' +
-							'<th scope="row">3</th>' +
-							'<td>Alberta</td>' +
-							'<td>Einstein</td>' +
-							'<td>Diseño</td>' +
-							'</tr>' +
-							'<tr>' +
-							'<th scope="row">4</th>' +
-							'<td>Elizabeth</td>' +
-							'<td>Mary</td>' +
-							'<td>Psicología</td>' +
-							'</tr>' +
-							'</tbody>' +
-							'</table>'
-						});
-					}
-				}
-			}
-    }
-  },
-	//DATA
-  series: [{
-    name: 'Pasantías',
-    colorByPoint: true,
-    data: [{
-      name: 'Pasantías terminadas con defensa disponible',
-      y: 35.98,
-			cantidadPasantias: 350,
-      sliced: true,
-      selected: true
-  	}, {
-    	name: 'Pasantías terminadas sin defensa disponible',
-    	y: 19.14,
-			cantidadPasantias: 190
-  	}, {
-    	name: 'Pasantías no terminadas',
-      y: 44.88,
-			cantidadPasantias: 440
-    }]
-  }]
-});
-
-//Grafico de empresas en convenio, proceso y sin convenio
-window.chart = new Highcharts.chart({
-	//EN DONDE UBICARLO
-	chart: {
-		type: 'pie',
-		renderTo: 'empresas',
-		height: (9 / 16 * 100) + '%',
-		plotBackgroundColor: null,
-		plotBorderWidth: null,
-		plotShadow: false,
-	},
-	//TITULO
-	title: {
-		text: 'Proceso de convenio de empresas para pasantías ',
-		style: {
-			fontSize: '22px'
-		}
-	},
-	//TOOLTIP
-	tooltip: {
-			pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-	},
-	//BOTONES DE DESCARGA
-	exporting: {
-		enabled: true,
-		csv: {
-			dateFormat:'%A, %b %e, %Y'
-		}
-	},
-	//SACAR CREDITOS
-	credits: {
-		enabled: false
-	},
-	//LABEL EJE X
-	xAxis: {
-		type: 'category',
-		labels: {
-			style: {
-				fontSize: '14px',
-				fontWeight: 'bold'
-			}
-		}
-	},
-	//LABEL EJE Y
-	yAxis: {
-		min: 0,
-		max: 100,
+/*
+chart_RenderTo -> id del elemento en donde se mostrara este grafico STRING
+title_Text -> titulo del grafico STRING
+series_Name -> nombre general de los datos STRING
+data_Attributes1 -> [nombre, valorNumerico, valorPorcentaje, detalleDatos] STRING, INT, FLOAT, OBJECT
+data_Attributes2 -> [nombre, valorNumerico, valorPorcentaje, detalleDatos] STRING, INT, FLOAT, OBJECT
+	*****detalleDatos -> funcion que da HTML a mostrar cuando se clickea un elemento del grafico
+*/
+const pieChartConstructor = (chart_RenderTo, title_Text, series_Name, data_Attributes1, data_Attributes2) => {
+	//Grafico de torta genérico
+	window.chart = new Highcharts.chart({
+		chart: {
+			//tipo
+			type: 'pie',
+			//donde ubicar
+			renderTo: chart_RenderTo,
+			//tamaño
+			height: (9 / 16 * 100) + '%',
+			plotBackgroundColor: null,
+			plotBorderWidth: null,
+			plotShadow: false
+		},
 		title: {
-      text: 'Porcentaje de postulantes',
+			//titulo
+			text: title_Text,
+			//estilos
 			style: {
-				fontSize: '14px',
-				fontWeight: 'bold'
+				fontSize: '22px'
 			}
 		},
-		labels: {
-			style: {
-				fontSize: '14px'
+		//lo que muestra al hover
+		tooltip: {
+			pointFormat: '{series.name}: <b>{point.percentage:.2f}%</b>'
+		},
+		//descarga habilitada
+		exporting: {
+			enabled: true,
+			csv: {
+				//fecha
+				dateFormat:'%A, %b %e, %Y'
 			}
-		}
-	},
-	//COLORES Y LABEL DE CADA PARTE
-  plotOptions: {
-    pie: {
-      allowPointSelect: true,
-      dataLabels: {
-        enabled: true,
-        format: '<b>{point.name}</b>: {point.cantidadEmpresas} ',
-        style: {
-          color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black',
-					fontSize: '1em'
-        },
-      }
-    },
-		series: {
-			cursor: 'pointer',
-			point: {
-				events: {
-					click: function (e) {
-						hs.htmlExpand(null, {
-							pageOrigin: {
-								x: e.pageX || e.clientX,
-								y: e.pageY || e.clientY
-							},
-							headingText: this.series.data[this.x].name,
-							maincontentText: this.detalleDatos
-						});
+		},
+		//no usar creditos en el grafico
+		credits: {
+			enabled: false
+		},
+		//opciones del grafico
+		plotOptions: {
+			pie: {
+				allowPointSelect: true,
+				dataLabels: {
+					enabled: true,
+					format: '<b>{point.name}</b>: {point.percentage:.2f}%',
+					style: {
+						color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black',
+						fontSize: '1em'
+					}
+				}
+			},
+			series: {
+				cursor: 'pointer',
+				point: {
+					events: {
+						click: function (e) {
+							hs.htmlExpand(null, {
+								pageOrigin: {
+									x: e.pageX || e.clientX,
+									y: e.pageY || e.clientY
+								},
+								//lo que se mostrara al clickear
+								headingText: this.series.data[this.x].name,
+								maincontentText:this.detalleDatos
+							});
+						}
 					}
 				}
 			}
-		}
-	},
-	//DATA
-  series: [{
-    name: 'Empresas',
-    colorByPoint: true,
-    data: [{
-      name: 'Empresas con convenio',
-			cantidadEmpresas: @json($estadisticasEmpresas['empresasValidadasCount']),
-      y: @json($estadisticasEmpresas['empresasValidadasCount']) / @json($estadisticasEmpresas['empresasTotal']) * 100,
-			detalleDatos: detalleDatosEmpresas(@json($estadisticasEmpresas['empresasValidadas']))
-    }, {
-      name: 'Empresas en proceso',
-			cantidadEmpresas: @json($estadisticasEmpresas['empresasEnProcesoCount']),
-			y: @json($estadisticasEmpresas['empresasEnProcesoCount']) / @json($estadisticasEmpresas['empresasTotal']) * 100,
-			detalleDatos: detalleDatosEmpresas(@json($estadisticasEmpresas['empresasEnProceso']))
-    }, {
-      name: 'Empresas sin convenio',
-			cantidadEmpresas: @json($estadisticasEmpresas['empresasNoValidadasCount']),
-			y: @json($estadisticasEmpresas['empresasNoValidadasCount']) / @json($estadisticasEmpresas['empresasTotal']) * 100,
-			sliced: true,
-			selected: true,
-			detalleDatos: detalleDatosEmpresas(@json($estadisticasEmpresas['empresasNoValidadas']))
-    }]
-  }]
-});
-</script>
+		},
+		//todos los datos del grafico
+		series: [{
+			name: series_Name,
+			colorByPoint: true,
+			data: [{
+				name: data_Attributes1[0],
+				y: data_Attributes1[1],
+				value: data_Attributes1[2],
+				detalleDatos: data_Attributes1[3],
+				sliced: true,
+				selected: true
+			}, {
+				name: data_Attributes2[0],
+				y: data_Attributes2[1],
+				value: data_Attributes2[2],
+				detalleDatos: data_Attributes2[3],
+			}]
+		}]
+	});
+}
+
+//Pie chart de empresas
+pieChartConstructor('empresasEnConvenio', 'Convenios de empresas', 'Empresas',
+	//data_Attributes1
+	['Empresas con convenio',
+	@json($estadisticasEmpresas['empresasValidadasCount']),
+	@json($estadisticasEmpresas['empresasPorcentajeValidadas']),
+	detalleDatosEmpresas(@json($estadisticasEmpresas['empresasValidadas']))],
+	//data_Attributes2
+	['Empresas sin convenio',
+	@json($estadisticasEmpresas['empresasNoValidadasCount']),
+	@json($estadisticasEmpresas['empresasPorcentajeNoValidadas']),
+	detalleDatosEmpresas(@json($estadisticasEmpresas['empresasNoValidadas']))]);
+
+//Pie chart de proyectos validados vs no validados
+pieChartConstructor('proyectosValidados', 'Estado de los proyectos', 'Proyectos',
+	//data_Attributes1
+	['Proyectos validados',
+	@json($estadisticasPasantias['pasantiasPaso4Count']),
+	@json($estadisticasPasantias['pasantiasPaso4Count']) / @json($estadisticasPasantias['pasantiasTotal']) * 100,
+	detalleDatosAlumnos(@json($estadisticasPasantias['alumnosPasantiaPaso4']))],
+	//data_Attributes2
+	['Proyectos no validados',
+	@json($estadisticasPasantias['pasantiasPaso1Count']) + @json($estadisticasPasantias['pasantiasPaso2Count']) + @json($estadisticasPasantias['pasantiasPaso3Count']),
+	(@json($estadisticasPasantias['pasantiasPaso1Count']) + @json($estadisticasPasantias['pasantiasPaso2Count']) + @json($estadisticasPasantias['pasantiasPaso3Count'])) / @json($estadisticasPasantias['pasantiasTotal']) * 100,
+	detalleDatosAlumnos(@json($estadisticasPasantias['alumnosPasantiaPaso1']))]);
+	</script>
 @endsection
