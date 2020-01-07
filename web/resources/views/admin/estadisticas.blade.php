4db1c58906c98d7cbfe5c39b71f01ded10164101
Ignacio Zenteno Smith <31903811+IgnacioZentenoSmith@users.noreply.github.com>
2020-01-06 22:42:51 -0300

    #139 Creacion de tabla descargable Alumnos
    Logica que permite la creacion de tablas descargables de Alumnos dentro del modal, con opcion para descargar todo o seleccionado.

--- web/resources/views/admin/estadisticas.blade.php
+++ web/resources/views/admin/estadisticas.blade.php
@@ -404,33 +404,75 @@ tabla_empresasEnConvenio(@json($estadisticasEmpresas['empresasValidadas']), 'g_e
 tabla_empresasEnConvenio(@json($estadisticasEmpresas['empresasNoValidadas']), 'g_empresasConvenio_Novalidas', 'bodyModal_EmpresasNoValidas');
 
 
-//Funcion para obtener todo el html dinamico a mostrar cuando se clickea una barra de alumnos
-const detalleDatosAlumnos = (JSONdatosAlumnos) => {
+const tabla_detalleAlumnos = (JSONdatosAlumnos, idTabla, idBodyModal) => {
 	//Tabla HTML a desplegar
 	let tablaHTMLAlumnos =
-		'<table class="table table-striped">' +
+	'<table id="'+idTabla+'" class="table table-striped shadow-lg" data-show-export="true" data-pagination="true" data-click-to-select="true" data-toolbar="#toolbar" data-show-toggle="true" data-show-columns="true" data-sortable="true" data-toggle="table" data-search="true" data-live-search="true">' +
 		'<thead>' +
 		'<tr>' +
+		'<th scope="col">id</th>'+
 		'<th scope="col">#</th>' +
 		'<th scope="col">Nombre</th>' +
 		'<th scope="col">Apellido</th>' +
 		'<th scope="col">Email</th>' +
 		'</tr>' +
-		'</thead>';
+		'</thead>'+
+		'<tbody>';
 	const datosAlumnos = JSONdatosAlumnos;
 	for (let i = 0; i < datosAlumnos.length; i++){
 		let datosAlumno = datosAlumnos[i];
 		tablaHTMLAlumnos += '<tr>' +
+			'<td></td>' +
 			'<th scope="row">'+ (i + 1) +'</th>' +
 			'<td>'+ datosAlumno['nombres'] +'</td>' +
 			'<td>'+ datosAlumno['apellidoPaterno'] +'</td>' +
 			'<td>'+ datosAlumno['email'] +'</td>' +
 			'</tr>';
 	}
-	tablaHTMLAlumnos += '<tbody>' +
-		'</tbody>' +
-		'</table>';
-	return tablaHTMLAlumnos;
+	tablaHTMLAlumnos +='</tbody>' + '</table>';
+	//Toolbar HTML para la tabla
+	const toolbarAlumnos =
+		'<div id="'+idTabla+'toolbar" class="select">'+
+			'<select class="form-control">'+
+				'<option value="all">Exportar todo</option>'+
+				'<option value="selected">Exportar seleccionado</option>'+
+			'</select>'+
+		'</div>';
+
+	//ID del BODY del modal
+	let modalBodyDetalleAlumnos = document.getElementById(idBodyModal);
+	//Primero toolbar, luego tabla
+	modalBodyDetalleAlumnos.innerHTML += toolbarAlumnos;
+	modalBodyDetalleAlumnos.innerHTML += tablaHTMLAlumnos;
+	//JQuery forma de descarga de tabla y cuales columnas
+	const $tablaAlumnos = $('#'+idTabla);
+	$(function() {
+		$('#'+idTabla+'toolbar').find('select').change(function () {
+			$tablaAlumnos.bootstrapTable('destroy').bootstrapTable({
+				exportDataType: $(this).val(),
+				exportTypes: ['csv', 'excel', 'pdf'],
+				columns: [
+					{
+						field: 'state',
+						checkbox: true,
+						visible: $(this).val() === 'selected'
+					},  {
+						field: '#',
+						title: '#'
+					}, {
+						field: 'Nombre',
+						title: 'Nombre'
+					}, {
+						field: 'Apellido',
+						title: 'Apellido'
+					}, {
+						field: 'Email',
+						title: 'Email'
+					}
+				]
+			})
+		}).trigger('change');
+	});
 }
 
 
